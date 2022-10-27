a512e1c6efc5553cba7365c289c12170039b2aa5
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-27 03:09:49 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -128,11 +128,12 @@ def pregunta_04():
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
     df = pd.read_csv("gm_2008_region.csv")
 
+    
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = df["fertility"].values.reshape(139,1)
+    X_fertility = df['fertility'].values.reshape(139, 1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = df["life"].values.reshape(139,1)
+    y_life = df['life'].values.reshape(139, 1)
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
@@ -141,18 +142,18 @@ def pregunta_04():
         y_life,
         test_size=0.2,
         random_state=53,
-    )
+    ) 
 
     # Cree una instancia del modelo de regresión lineal
     linearRegression = LinearRegression()
 
     # Entrene el clasificador usando X_train y y_train
-   linearRegression.fit(X_train, y_train)
+    linearRegression.fit(X_train, y_train)
 
     # Pronostique y_test usando X_test
-    y_pred =linearRegression.predict(X_test)
+    y_pred = linearRegression.predict(X_test)
 
     # Compute and print R^2 and RMSE
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
-    rmse = np.sqrt(mean_squared_error(y_test,y_pred))
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
